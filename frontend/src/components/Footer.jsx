a8a63a1d43bc44ba2c22f2fbef50c85de8be6e9d
shashwatshukla11 <shashwatshukla0511@gmail.com>
2024-10-20 22:54:32 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6 text-justify'>“CarePath: Your Health, Our Priority” is a compassionate and streamlined service that guides individuals on their wellness journey. From appointment scheduling to personalized care plans, we prioritize your well-being every step of the way. 🌿🏥</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91-8077215298</li>
+            <li>shashwatshukla0511@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ CarePath.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
